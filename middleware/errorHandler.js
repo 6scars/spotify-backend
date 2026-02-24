381e1fdbb214704aef9ca6efd03027494d505109
6scars <zasalatol@o2.pl>
2026-02-24 13:34:35 +0100

    Chore(errorHandler.js modular) - Make error Handler more modular.

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,23 +3,39 @@
 export default class AppError extends Error{
     constructor(message, status){
         super(message);
-        this.status = status || 500
+        this.status        = status || 500
         this.isOperational = true;
     }
 }
 
 
 export function errorHandler(err, req, res, next) {
-  console.log(process.env.NODE_ENV)
-  if(err.status < 500){
-    res.status(err.status).json({
-      message: err.message
-    });
-  }
-  if(err.status >= 500){
-    res.status(500).json({
-      message: "something goes wrong"
-    })
-  }
+  if(process.env.DEVELOPMENT === "YES"){
+      console.log(process.env.DEVELOPMENT)
+      errorDevelopment(err, res)
+    }else{
+      errorProduction (err, res)
+    } 
 }
 
+
+
+
+function errorDevelopment(err, res){
+      res.status(err.status).json({
+      message: err.message
+    });
+}
+
+function errorProduction (err, res){
+     if(err.status < 500){
+        res.status(err.status).json({
+          message: err.message
+        });
+      }
+      if(err.status >= 500){
+        res.status(500)       .json({
+          message: "something goes wrong, try again later or contact with us"
+        })
+  }
+}
