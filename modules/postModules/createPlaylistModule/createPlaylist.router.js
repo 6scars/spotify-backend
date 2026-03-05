5becc31297de41e095ea6326c2ec771a61362019
6scars <zasalatol@o2.pl>
2026-03-05 17:10:26 +0100

    Refactor(imports) - import styles are equal (from)

--- modules/postModules/createPlaylistModule/createPlaylist.router.js
+++ modules/postModules/createPlaylistModule/createPlaylist.router.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import createPlaylist from './createPlaylist.controller.js'
+import express              from 'express'
+import createPlaylist       from './createPlaylist.controller.js'
 
 const router = express.Router()
 
